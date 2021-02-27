aee57c508a03c96bba2c7ee80dbd801deaec0bc6
Burton <32318847+Burton85@users.noreply.github.com>
2021-02-27 00:32:13 -0400

    pwa

---
+++ precache-manifest.cfdcbd49f5fbbe2fd6c0dd483ce51436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647e7189e4a7b895ec9d",
+    "url": "/vuetodo/css/app.83604d20.css"
+  },
+  {
+    "revision": "b6c000ef8e5a6b28f5672f6873f136d9",
+    "url": "/vuetodo/index.html"
+  },
+  {
+    "revision": "647e7189e4a7b895ec9d",
+    "url": "/vuetodo/js/app.01ac1604.js"
+  },
+  {
+    "revision": "bf353a2aef5506fa917c",
+    "url": "/vuetodo/js/chunk-vendors.614a1f9a.js"
+  },
+  {
+    "revision": "40f5ed99c7ab54c4e7ac1d1aeaa22cf2",
+    "url": "/vuetodo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuetodo/robots.txt"
+  }
+]);
